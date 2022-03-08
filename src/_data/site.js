f9f679d3c2f60deea42f53f69ff0b61d6c4357ab
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2022-03-07 21:36:04 -0500

    Setting up a VERY opinionated Eleventy Starter

---
+++ src/_data/site.js
@@ -0,0 +1,17 @@
+module.exports = (info) => {
+	return {
+		lang: "en-US",
+		github: {
+			build_revision: process.env.MY_GITHUB_RUN_ID || 1.0,
+			build_sha: process.env.GITHUB_SHA || 1,
+		},
+		site_url: process.env.DOMAIN,
+		site_domain: process.env.DOMAIN_NAME,
+		site_name: process.env.SITE_NAME,
+		description: process.env.DESCRIPTION,
+		featuredImage: process.env.BASIC_IMAGE,
+		author: process.env.PRIMARY_AUTHOR,
+		authorPhoto:
+			"https://raw.githubusercontent.com/AramZS/aramzs.github.io/master/_includes/Aram-Zucker-Scharff-square.jpg",
+	};
+};
